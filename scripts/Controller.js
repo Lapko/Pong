c2efbd97b4afee7726902c8ab253c8d7bd2be49d
Lapko <lapomasar@icloud.com>
2017-04-19 16:48:19 +0200

    version2

--- scripts/Controller.js
+++ scripts/Controller.js
@@ -1,4 +1,6 @@
 class Controller{
+	
+	
 	movePlayer(keyPressed){
 			//alert(keyPressed);
 			
@@ -15,10 +17,11 @@ class Controller{
 				},1000/60);
 				break;
 			case "c":
+				//alert("tried to kill");
 				this.kill;
 				break;
 			case "Escape":
-				this.reset();
+				this.resett();
 			default:
 				break;
 		}	
@@ -27,52 +30,49 @@ class Controller{
 	
 	moveEnemy(){
 		//console.log("hero Y: " + playerMain.posY + " enemy pos Y: " + playerEnemy.posY + " ball X: " + ball.posX + " ball Y: " + ball.posY);
-		if(ball.posY > (playerEnemy.posY + playerEnemy.recHeight/2))
-			playerEnemy.recDraw(playerEnemy.posY + 10);
-		else 
-			playerEnemy.recDraw(playerEnemy.posY - 10);
+		if(ball.posX > window.innerWidth/3){
+			if(ball.posY > (playerEnemy.posY + playerEnemy.recHeight/3))
+				playerEnemy.recDraw(playerEnemy.posY + 7);
+			else if(ball.posY < (playerEnemy.posY + playerEnemy.recHeight*2/3))
+				playerEnemy.recDraw(playerEnemy.posY - 7);
+		}
+		else playerEnemy.recDraw(playerEnemy.posY);
 	}
 	//sadasda
 	moveBall(){
-		if(ball.posX > 100 && ball.posX < palette.canvas.width - 200)
-			ball.posX += ball.velX;
-		else{
-			//alert("menim smer X");
-			if((playerMain.posY > ball.posY)||(playerMain.posY + (playerMain.recHeight*0.3333) < ball.posY)){
-				ball.velX *= -1;
-				if(ball.velY < 0){
-					ball.velY *= -1;
-					ball.velX *= 1.2;
-				}
-				else{
-					ball.velY *= 1.3;
-					ball.velX *= 0.8;
-				}
-				ball.posX += ball.velX;
-			} else if ((playerMain.posY + (playerMain.recHeight*0.3333) > ball.posY)||(playerMain.posY + (playerMain.recHeight*0.6666) < ball.posY)){
-				ball.velX *= -1.3;
-				ball.posX += ball.velX;
-			} else{
-				ball.velX *= -1;
-				if(ball.velY < 0){
-					ball.velX *= 0.8;
-					ball.velY *= 1.3;
-				}
-				else{
-					ball.velX *= 1.2;
-					ball.velY *= -1;
-				}
-				ball.posX += ball.velX;
-			}
-		}
-		if(ball.posY > 50 && ball.posY < palette.canvas.height - 100)
-			ball.posY += ball.velY;
-		else{
-			//alert("menim smer Y");
-			ball.velY *= -1;
-			ball.posY += ball.velY;
-		}
+		this.maxAngle = 5*Math.PI/12;
 		
+		if(ball.posX-ball.radius + ball.velX > (playerMain.posX + playerMain.recWidth))
+			ball.posX += ball.velX;
+		else if(((playerMain.posY < ball.posY)&&(playerMain.posY + playerMain.recHeight > ball.posY))&&ball.velX < 0){
+			this.collisionY = ((playerMain.posY + playerMain.recHeight/2) - ball.posY)/(playerMain.recHeight/2);
+			this.newAngle = this.collisionY * this.maxAngle;
+			ball.velX = Math.abs(15*Math.cos(this.newAngle));
+			ball.velY = 15*-Math.sin(this.newAngle);
+			alert(ball.velX);
+			ball.posX += ball.velX;
+			ball.posY += ball.velY;
+		}
+
+		if(ball.posX + ball.radius + ball.velX < playerEnemy.posX)
+			ball.posX += ball.velX;
+		else if(((playerEnemy.posY < ball.posY)&&(playerEnemy.posY + playerEnemy.recHeight > ball.posY))&&ball.velX > 0){
+			this.collisionY = (playerEnemy.posY + playerEnemy.recHeight/2 - ball.posY)/(playerEnemy.recHeight/2);
+			this.newAngle = this.collisionY * this.maxAngle;
+			ball.velX = -Math.abs(15*Math.cos(this.newAngle));
+			ball.velY = 15*-Math.sin(this.newAngle);
+			alert(ball.velX);
+			ball.posX += ball.velX;
+			ball.posY += ball.velY;
+		}
+			//console.log(20*Math.sin(this.newAngle) + " collisionY: " + this.collisionY + " newAngle: " + this.newAngle);
+		//if(ball.posY - ball.radius > 50 && ball.posY + ball.radius < palette.canvas.height - 100)
+			//ball.posY += ball.velY;
+		if(ball.posY - ball.radius < 50)
+			ball.velY = Math.abs(ball.velY);
+		else if(ball.posY + ball.radius > palette.canvas.height)
+			ball.velY = -Math.abs(ball.velY);
+		ball.posY += ball.velY;
 		ball.drawBall();
 	}
 	
@@ -82,26 +82,28 @@ class Controller{
 		//console.log((playerMain.posY + playerMain.recHeight < ball.posY));
 		if((ball.posX - ball.radius < (playerMain.posX + playerMain.recWidth)) && ((playerMain.posY > ball.posY)||(playerMain.posY + playerMain.recHeight < ball.posY))){
 			enemyScore++;
-			this.reset();
+			this.resett();
 		}
 		if((ball.posX + ball.radius > (playerEnemy.posX)) && ((playerEnemy.posY > ball.posY)||(playerEnemy.posY + playerEnemy.recHeight < ball.posY))){
 			playerScore++;
-			this.reset();
+			this.resett();
 		}
 		//console.log("nazdar");
 	}
 	
 	kill(){
-		clearInterval(ballSpeedInt);
+		//clearInterval(ballSpeedInt);
 		clearInterval(mainInt);
 		clearInterval(this.playerInt);
 	}
 	
-	reset(){
-		clearInterval(ballSpeedInt);
+	resett(){
+		//clearInterval(ballSpeedInt);
 		clearInterval(mainInt);
 		//running = false;
-		document.getElementById("Player").innerHTML = "";
+		//playerScore = 0;
+		//enemyScore = 0;
+		document.getElementById("Score").innerHTML = "";
 		start();
 		main();
 	}
--- scripts/drawBall.js
+++ scripts/drawBall.js
@@ -4,18 +4,19 @@ class drawBall{
 		//alert(this.decideWay);
 		this.posX = (palette.canvas.width-500)/2;
 		this.posY = palette.canvas.height/2;
-		this.radius = 50;
-		this.velX = Math.random()*10 + 5;
+		this.radius = 20;
+		this.velX = 10;
 		if(this.decideWay > 1)
-			this.velY = Math.random()*10 + 3;
+			this.velY = 10;
 		else
-			this.velY = (Math.random()*10 + 3)* -1;
+			this.velY = -10;
 		//this.angle = 45;
 		
 		this.drawBall();
 	}
 	
 	drawBall(){
+		//console.log("VelX : " + this.velX + " VelY : " + this.velY );
 		palette.beginPath();
 		palette.arc(this.posX,this.posY,this.radius,0,2*Math.PI,false);
 		palette.lineWidth = 5;
